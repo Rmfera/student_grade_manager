c24b989e3dbfd2a21c8593978c7fe86b97658f21
Reinaldo Ferreira <reinaldo.mferreira@yahoo.com.br>
2025-11-09 16:37:06 -0300

    PUT and DELETE inserted

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const server = http.createServer((request, response) => {
   });
 
   request.on("end", () => {
+    const id = url.split("/")[2];
     if (url === "/grades" && method === "GET") {
       response.writeHead(200, { "Content-Type": "application/json" });
       response.end(JSON.stringify(grades));
@@ -22,6 +23,29 @@ const server = http.createServer((request, response) => {
       grades.push(newGrade);
       response.writeHead(201, { "Content-Type": "application/json" });
       response.end(JSON.stringify(newGrade));
+    } else if (url.startsWith("/grades/") && method === "PUT") {
+      const { studentName, subject, grade } = JSON.parse(body);
+      const gradeToUpdate = grades.find((grade) => grade.id === id);
+      if (gradeToUpdate) {
+        gradeToUpdate.studentName = studentName;
+        gradeToUpdate.subject = subject;
+        gradeToUpdate.grade = grade;
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(gradeToUpdate));
+      } else {
+        response.writeHead(404, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ message: "Grade not found" }));
+      }
+    } else if (url.startsWith("/grades/") && method === "DELETE") {
+      const index = grades.findIndex((grade) => grade.id === id);
+      if (index !== -1) {
+        grades.splice(index, 1);
+        response.writeHead(204);
+        response.end();
+      } else {
+        response.writeHead(404, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ message: "Grade not found" }));
+      }
     } else {
       response.writeHead(400, { "Content-Type": "application/json" });
       response.end(JSON.stringify({ message: "Route not found" }));
